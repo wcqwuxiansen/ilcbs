bdc333c97930f0b000764efa775f68477e586b38
tom_glb <tom_glb@atguigu.com>
2021-08-25 18:10:47 +0800

    on 2021/8/25

--- ilcbs_web/src/main/java/com/heima/Demo3Servlet.java
+++ ilcbs_web/src/main/java/com/heima/Demo3Servlet.java
@@ -11,7 +11,7 @@ public class Demo3Servlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+		System.out.println("hello git");
 	}
 
 	@Override
