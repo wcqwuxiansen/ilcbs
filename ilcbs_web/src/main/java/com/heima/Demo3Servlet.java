8d335c2f3a003a4fd73862fc590e7ec94a798896
tom_glb <tom_glb@atguigu.com>
2022-02-25 17:33:57 +0800

    update 2022/2/25

--- ilcbs_web/src/main/java/com/heima/Demo3Servlet.java
+++ ilcbs_web/src/main/java/com/heima/Demo3Servlet.java
@@ -14,6 +14,7 @@ public class Demo3Servlet extends HttpServlet {
 		System.out.println("hello git");
 		System.out.println("hello git");
 		System.out.println("hello git");
+		System.out.println("hello git2");
 	}
 
 	@Override
