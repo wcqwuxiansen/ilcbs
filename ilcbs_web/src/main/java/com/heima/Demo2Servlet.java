df6b8cf70377e4e16f6accda022ae6cbb72ce390
wcqwuxiansen <89242284+wcqwuxiansen@users.noreply.github.com>
2022-02-28 16:26:26 +0800

    Update Demo2Servlet.java

--- ilcbs_web/src/main/java/com/heima/Demo2Servlet.java
+++ ilcbs_web/src/main/java/com/heima/Demo2Servlet.java
@@ -11,7 +11,7 @@ public class Demo2Servlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+		System.out.println("111111111111111111111111111");
 		super.doGet(req, resp);
 	}
 
