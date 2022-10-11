32a07ebbcae7e95aff2a527e154be9bea5cc97f6
wcqwuxiansen <89242284+wcqwuxiansen@users.noreply.github.com>
2022-10-11 17:48:21 +0800

    Update Demo3Servlet.java

--- ilcbs_web/src/main/java/com/heima/Demo3Servlet.java
+++ ilcbs_web/src/main/java/com/heima/Demo3Servlet.java
@@ -11,7 +11,7 @@ public class Demo3Servlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+		resp.getWriter().write("hello git111111111111111111111111111111111 ");
 	}
 
 	@Override
